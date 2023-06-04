013b6979b290cb4d14e30594ead8b81397f03ce2
sinjeeli <sinjeeli@gmail.com>
2023-06-04 08:55:52 -0400

    updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('test');
+
+var nameRef = document.getElementById('name');
+console.log(nameRef);
+//Create a variable to reference the “Name” <input type="text"> element and log the variable out to the console to ensure the correct element is being referenced.
+
+nameRef.focus();
+//Use the variable and the .focus() method to add the focus state to the element.
+
+
+//
+
