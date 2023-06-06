09e4a6c0c4c1497e7d87565390f47d19898dba18
sinjeeli <sinjeeli@gmail.com>
2023-06-06 00:38:53 -0400

    good so far

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 // console.log('test');
 
+var nameRef = document.getElementById('name');
+console.log(nameRef);
+
+nameRef.focus();
+//title is for job role
+var title = document.getElementById('title');
+console.log(title);
+var otherJobRole = document.getElementById('other-job-role');
+console.log(otherJobRole);
+//
+otherJobRole.style.display = "none";
+//
+title.addEventListener('change', (event) => {
+
+
+    event.target.value;
+    console.log(title);
+    console.log(event.target.value);
+
+});
